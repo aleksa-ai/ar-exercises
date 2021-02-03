2a8761656b185afcb50f28acbf93a3b5121717cd
Aleksa Gavrilovic <lks_g@live.ca>
2021-02-03 10:47:02 -0500

    Exercise 3 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -8,9 +8,6 @@ puts "----------"
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
 
-# Checking if it prints the name for fun
-# puts @store1.name
-
 @store1.update(name: 'Surrey', annual_revenue: 1200000, mens_apparel: true, womens_apparel: true) 
-# puts @store1.name
+
 
